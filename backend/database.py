f144b619fdf9de3e0f8719a69703b1b123f483b1
SolovevaYuliya <soloveva.iuliia89@yandex.ru>
2026-03-28 02:19:18 +0300

    Add files via upload

--- backend/database.py
+++ backend/database.py
@@ -1,33 +1,31 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.pool import NullPool
+from sqlalchemy.pool import NullPool # Импортируем это!
 
-# 1. Получаем URL
+# Получаем URL
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://postgres:1234@localhost:5432/Catopoly_db"
 )
 
-# 2. Фикс для Render
+# Фикс для Render
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# 3. Настраиваем аргументы подключения
+# Настройки для облака (Supabase)
 connect_args = {}
-
-# Если мы НЕ на локалке, добавляем SSL и фикс для пулера
 if "localhost" not in SQLALCHEMY_DATABASE_URL:
+    # ИЗМЕНЕНО ТУТ: Добавлен prepare_threshold, чтобы работали ТРЕЙДЫ
     connect_args = {
         "sslmode": "require",
-        "prepare_threshold": 0  # <--- АККУРАТНО ДОБАВИЛИ СЮДА
+        "prepare_threshold": 0
     }
 
-# 4. Создаем движок
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args=connect_args,
-    poolclass=NullPool
+    poolclass=NullPool # ЭТО ГЛАВНОЕ: закрываем соединение сразу после запроса
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
